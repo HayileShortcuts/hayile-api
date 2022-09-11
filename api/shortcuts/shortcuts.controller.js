76fcc12c58dfff4d4ed175ee46613cf4877dc26b
J0suu <josuherdroid@gmail.com>
2022-09-11 15:51:59 +0100

    fix error in catch

--- api/shortcuts/shortcuts.controller.js
+++ api/shortcuts/shortcuts.controller.js
@@ -1,21 +1,12 @@
 const ShortcutModel = require("./shortcut.model");
 
-// Obtener un usuario por su ID
-async function getEnvironment(req, res) {
+getEnvironment = async (req, res) => {
   try {
-    ShortcutModel.findOne({ name: req.params.id }).then((result) =>
-      res.status(200).send(result.program),
-    );
-  } catch (error) {
-    res.status(500).send(err);
+    const shortcut = await ShortcutModel.findOne({ name: req.params.id });
+    res.status(200).send(shortcut.program);
+    } catch (error) {
+    res.status(500).send(error);
   }
 }
 
-// async function post(req, res) {
-//   try {
-//     const { name } = req.body;
-//     const response = await ShortcutModel.create({ name });
-//     res.status(200).send({ response });
-//   } catch (error) {}
-// }
 module.exports = { getEnvironment };
