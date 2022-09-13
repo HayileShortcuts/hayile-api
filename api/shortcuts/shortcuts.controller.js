cfa235643fb68bdb09f908a4461a75ea9b41f5d0
J0suu <josuherdroid@gmail.com>
2022-09-13 13:28:44 +0100

    implement redis
    Rama creada para implementar redis, aun no va.

--- api/shortcuts/shortcuts.controller.js
+++ api/shortcuts/shortcuts.controller.js
@@ -1,9 +1,28 @@
 const ShortcutModel = require("./shortcut.model");
+const redis = require("redis")
+
+const client = redis.createClient({
+  url: 'redis://default:hayQuePonerElPassword@redis-16403.c268.eu-west-1-2.ec2.cloud.redislabs.com:16403'
+  });
+
 
 getEnvironment = async (req, res) => {
   try {
+    
+    client.connect();
+
+    client.on('error', (err) => console.log('Redis Client Error', err));
+
     const shortcut = await ShortcutModel.findOne({ name: req.params.id });
-    res.status(200).send(shortcut.program);
+
+    await client.set(req.params.id, JSON.stringify(shortcut.program), (err, reply) => {
+      if (err) console.log(err);
+
+      console.log(reply);
+
+      res.status(200).send(shortcut.program);
+    });
+
     } catch (error) {
     res.status(500).send(error);
   }
